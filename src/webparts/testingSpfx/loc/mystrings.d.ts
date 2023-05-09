d4bccff38a83b6c5c11340ad8ce6e36fa1de30c8
gabmor38 <gabriela.morenor@gmail.com>
2023-05-09 09:12:31 -0400

    initial commit

---
+++ src/webparts/testingSpfx/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ITestingSpfxWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'TestingSpfxWebPartStrings' {
+  const strings: ITestingSpfxWebPartStrings;
+  export = strings;
+}
