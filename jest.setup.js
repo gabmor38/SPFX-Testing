a25090584ed0edd0469407829893e95e68ce8caa
Vid <vid_github@zooid.org>
2023-06-07 12:01:41 -0400

    cleanup, add  jest-environment-jsdom

--- jest.setup.js
+++
@@ -1,5 +0,0 @@
-import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
-
-window.DEBUG = true; // for @pnp/spfx-controls-react
-
-initializeIcons(); // set up icons
--- src/webparts/testing/components/Testing.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import Testing from './Testing';
-
-describe('App component', () => {
- test('it renders', () => {
-   render(<Testing description='hihi' />);
- });
-})
