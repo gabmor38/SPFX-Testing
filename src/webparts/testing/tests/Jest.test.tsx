1700ad9e008a8d2d67aca5f5a73f2f38d43e7759
gabmor38 <gabriela.morenor@gmail.com>
2024-07-11 12:13:31 -0400

    added a test to the main component

--- src/webparts/testing/tests/Jest.test.tsx
+++ src/webparts/testing/tests/Jest.test.tsx
@@ -2,8 +2,9 @@
 
 
 import * as React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import TestChild2 from '../components/TestChild2';
+import Testing from '../components/Testing';
 
 
 describe ("Child Component", () => {
@@ -15,8 +16,20 @@ describe ("Child Component", () => {
     const {getByText} = render (<TestChild2/>);
     const text = getByText('Hello');
     expect(text).toBeDefined();
-  })
-})
+  });
+});
+
+describe('it renders an h3 element with the correct data-testid', () => {
+  it("should render Parent", () => {
+    render(<Testing description={''} isDarkTheme={false} environmentMessage={''} hasTeamsContext={false} userDisplayName={''} />);
+  });
+
+  it("should render an h3 element with the correct data-testid", () => {
+    render(<Testing description={''} isDarkTheme={false} environmentMessage={''} hasTeamsContext={false} userDisplayName={''} />);
+    const h3Element = screen.getByTestId('h3');
+    expect(h3Element.tagName).toBe('H3');
+  });
+});
 
 
 
